1dde20e709fccaf396fb425be87e579c3b80fd3d
kamaludumullahele <kamalchandrakumar@gmail.comgit config --global user.email kamalchandrakumara@gmail.com>
2026-07-23 11:50:30 -0600

    milestone project completion

---
+++ python files/week 3/week3_project/test_water_flow.py
@@ -0,0 +1,49 @@
+#import pytest and approx
+#import water_flow
+from pytest import approx
+import pytest
+import water_flow
+import math
+
+#calling test_water_column_height
+def test_water_colomn_height():
+
+    assert 0 + 3 * 0 / 4 == 0
+    assert 0 + 3 * 10 / 4 == 7.5
+    assert 25 + 3 * 0 /4 == 25
+    assert 48.3 + 3 * 12.8 / 4 == 57.9
+
+# calling test_pressure_gain_from_water_height function to 
+# to test the pressure values
+def test_pressure_gain_from_water_height():
+    assert 998.2 * 9.80665 * 0 / 1000 == approx(0, abs=0.001)
+    assert 998.2 * 9.80665 * 30.2 / 1000 == approx(295.628, abs=0.001)
+    assert 998.2 * 9.80665 * 50.0 / 1000 == approx(489.450, abs=0.001)
+
+# calling for test_pressure_loss_from_pipe to check the pressure_loss_from_pipe function
+
+def test_pressure_loss_from_pipe():
+    assert -0.018 * 0 * 998.2 * 1.75 ** 2 / 2000 * 0.048692 == approx(0, abs=0.001)
+    assert -0.0 * 200 * 998.2 * 1.75 ** 2 / 2000 * 0.048692 == approx(0, abs=0.001)
+    assert -0.018 * 200 * 998.2 * 0 ** 2 / 2000 * 0.048692 == approx(0, abs=0.001)
+    assert -0.018 * 200 * 998.2 * 1.75 ** 2 / (2000 * 0.048692) == approx(-113.008, abs=0.001)
+    assert -0.018 * 200 * 998.2 * 1.65 ** 2 / (2000 * 0.048692) == approx(-100.462, abs=0.001)
+    assert -0.013 * 1000 * 998.2 * 1.65 ** 2 / (2000 * 0.286870) == approx(-61.576, abs=0.001)
+    assert -0.013 * 1800.75 * 998.2 * 1.65 ** 2 / (2000 * 0.286870) == approx(-110.884, abs=0.001)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
