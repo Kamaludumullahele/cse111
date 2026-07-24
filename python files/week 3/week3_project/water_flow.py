ba081030c23f6b53bd5bb05635ba01fe2ac28721
kamaludumullahele <kamalchandrakumar@gmail.comgit config --global user.email kamalchandrakumara@gmail.com>
2026-07-24 16:48:56 -0600

    changes

--- python files/week 3/week3_project/water_flow.py
+++ python files/week 3/week3_project/water_flow.py
@@ -11,13 +11,14 @@ WATER_DYNAMIC_VISCOSITY = 0.0010016
 pressure_in_psi = 0
 def main():
     tower_height = float(input("Height of water tower (meters): "))
+
     tank_height = float(input("Height of water tank walls (meters): "))
     length1 = float(input("Length of supply pipe from tank to lot (meters): "))
     quantity_angles = int(input("Number of 90° angles in supply pipe: "))
     length2 = float(input("Length of pipe from supply to house (meters): "))
     
     water_height = water_column_height(tower_height, tank_height)
-    pressure = pressure_gain_from_water_height(water_height)
+    pressure = pressure_gain_from_water_height(water_height, WATER_DENSITY)
     diameter = PVC_SCHED80_INNER_DIAMETER
     friction = PVC_SCHED80_FRICTION_FACTOR
     velocity = SUPPLY_VELOCITY
@@ -41,11 +42,11 @@ def main():
 def water_column_height(tower_height, tank_height):
     return tower_height + 3 * tank_height / 4
 
-def pressure_gain_from_water_height(height, WATER_DENSITY):
+def pressure_gain_from_water_height(WATER_DENSITY ,water_column_height):
     #TODO: Need to implement
-    return WATER_DENSITY() * 9.80665 * (water_column_height) / 1000
+    return WATER_DENSITY * 9.80665 * water_column_height / 1000
 
-def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
+def pressure_loss_from_pipe(friction_factor, pipe_length, WATER_DENSITY, fluid_velocity, pipe_diameter):
     numerator = -friction_factor * pipe_length * WATER_DENSITY * fluid_velocity ** 2
     denominator = 200 * pipe_diameter
     return numerator / denominator
@@ -57,7 +58,7 @@ def reynolds_number(hydraulic_diameter, fluid_velocity, WATER_DENSITY):
       #TODO: Need to implement
       return hydraulic_diameter * fluid_velocity * WATER_DENSITY / 0.0010016
 
-def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
+def pressure_loss_from_pipe_reduction(reynolds_number,larger_diameter, smaller_diameter, WATER_DENSITY, fluid_velocity):
     k=(.1 + 50 / reynolds_number) * ((larger_diameter / smaller_diameter) ** 4 + 1)
     return -k * WATER_DENSITY * fluid_velocity ** 2 / 2000
 
